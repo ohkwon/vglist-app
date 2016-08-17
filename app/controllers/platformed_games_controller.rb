b87f134c20c77c9c755dc0badbd33028bbef70be
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-08-16 18:45:07 -0700

    add working date addition to platformed games on forms, add conditionals so show and index pages don't break when they don't have a platformed game, edit game show page to reflect release date and platforms

--- app/controllers/platformed_games_controller.rb
+++ app/controllers/platformed_games_controller.rb
@@ -6,10 +6,26 @@ class PlatformedGamesController < ApplicationController
 
   def create
 
+    if params[:release_date] == ""
+      release_date = ""
+    else
+      date_input = params[:release_date].split("-")
+      release_date = Date.new(date_input[0].to_i, date_input[1].to_i ,date_input[2].to_i)
+    end
+
     platformed_game = PlatformedGame.new(
-      game_id: params[:game_id],
-      platform_id: params[:platform_id]
+      game_id: params[:game][:game_id],
+      platform_id: params[:platform][:platform_id],
+      release_date: release_date
       )
+
+    if platformed_game.save
+      flash[:success] = "Added platform to Game!"
+      redirect_to '/games'
+    else
+      flash[:danger] = "Error!"
+      redirect_to '/platformed_games/new'
+    end
 
   end
 
