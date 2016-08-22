8c0a6f1510492f27009c1a4a8f0e200d29d1023e
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-08-21 17:04:55 -0700

    edit lowest deal method, no implementation to relevant views

--- app/models/game.rb
+++ app/models/game.rb
@@ -9,8 +9,14 @@ class Game < ApplicationRecord
   has_many :genred_games
   has_many :genres, through: :genred_games
 
-  def lowest_deal
-    platformed_games.first.deals.where(active: true).order(:price).first
+  def lowest_deals
+    platform_deals = []
+
+    platformed_games.each do |platformed_game|
+      platform_deals = << { platform_name: platformed_game.platform.name , price: platformed_game.deals.where(active: true).order(:price).first
+    end
+    #edited lowest_deals model method. have not yet fixed all relevant view pages
+
   end
   
 end
