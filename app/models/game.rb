2af2a6bf7a8d6292b67692ce76129ed9fa201abd
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-08-25 13:39:04 -0700

    add discount value method to reflect on games index view page

--- app/models/game.rb
+++ app/models/game.rb
@@ -14,7 +14,7 @@ class Game < ApplicationRecord
 
     self.platformed_games.each do |platformed_game|
       if platformed_game.deals.where(active: true).any?
-        platform_deals << { platform_name: platformed_game.platform.name , price: platformed_game.deals.where(active: true).order(:price).first.price, platformed_game_id: platformed_game.id }
+        platform_deals << { platform_name: platformed_game.platform.name , price: platformed_game.deals.where(active: true).order(:price).first.price, platformed_game_id: platformed_game.id, deal_id: platformed_game.deals.where(active: true).order(:price).first.id }
       end
     end
     
--- app/models/platformed_game.rb
+++ app/models/platformed_game.rb
@@ -4,7 +4,7 @@ class PlatformedGame < ApplicationRecord
   belongs_to :platform
   has_many :deals
 
-  def discount_value(deal)
+  def discount_value(deal) #if api has base price
     discount = (( self.base_price - deal.price ) / self.base_price ) * 100
     return discount.to_i
   end
