bae990e627b688d0b8286c500848df73ec6208b8
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-08-28 17:18:24 -0700

    add api to game model and games index action as poro, did not edit game index view page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,31 +4,40 @@ class GamesController < ApplicationController
 
   def index
 
-    sort_attribute = params[:sort_attribute]
-    sort_attribute_2 = params[:sort_attribute_2]
-    if sort_attribute == "platform"
-      if sort_attribute_2 == "1"
-        @games = Game.includes(:platformed_games).where('platformed_games.platform_id = 1').references(:platformed_games).order(:name) #ps4
-      elsif sort_attribute_2 == "2"
-        @games = Game.includes(:platformed_games).where('platformed_games.platform_id = 2').references(:platformed_games).order(:name) #xbone
-      elsif sort_attribute_2 == "3"
-        @games = Game.includes(:platformed_games).where('platformed_games.platform_id = 3').references(:platformed_games).order(:name) #wii u
-      elsif sort_attribute_2 == "4"
-        @games = Game.includes(:platformed_games).where('platformed_games.platform_id = 4').references(:platformed_games).order(:name) #pc
-      end
-    elsif sort_attribute == "genre"
-      if sort_attribute_2 == "1"
-        @games = Game.includes(:genred_games).where('genred_games.genre_id = 1').references(:genred_games).order(:name) #action-rpg
-      elsif sort_attribute_2 == "2"
-        @games = Game.includes(:genred_games).where('genred_games.genre_id = 2').references(:genred_games).order(:name) #fps
-      elsif sort_attribute_2 == "3"
-        @games = Game.includes(:genred_games).where('genred_games.genre_id = 3').references(:genred_games).order(:name) #survival-horror
-      elsif sort_attribute_2 == "4"
-        @games = Game.includes(:genred_games).where('genred_games.genre_id = 4').references(:genred_games).order(:name) #action-rpg
-      end
-    else
-      @games = Game.order(:name)
-    end
+    game_hashes = Unirest.get("https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=*&limit=50&offset=0.json",
+      headers:{
+        "X-Mashape-Key" => "arQcHPrN6ImshNKxsi3eTD0FYt7vp18kzZnjsnq60XoEEn991T",
+        "Accept" => "application/json"
+       }).body
+    @games = []
+    game_hashes.each do |game_hash|
+      @games << Game.new(game_hash)
+    end    
+    # sort_attribute = params[:sort_attribute]
+    # sort_attribute_2 = params[:sort_attribute_2]
+    # if sort_attribute == "platform"
+    #   if sort_attribute_2 == "1"
+    #     @games = Game.includes(:platformed_games).where('platformed_games.platform_id = 1').references(:platformed_games).order(:name) #ps4
+    #   elsif sort_attribute_2 == "2"
+    #     @games = Game.includes(:platformed_games).where('platformed_games.platform_id = 2').references(:platformed_games).order(:name) #xbone
+    #   elsif sort_attribute_2 == "3"
+    #     @games = Game.includes(:platformed_games).where('platformed_games.platform_id = 3').references(:platformed_games).order(:name) #wii u
+    #   elsif sort_attribute_2 == "4"
+    #     @games = Game.includes(:platformed_games).where('platformed_games.platform_id = 4').references(:platformed_games).order(:name) #pc
+    #   end
+    # elsif sort_attribute == "genre"
+    #   if sort_attribute_2 == "1"
+    #     @games = Game.includes(:genred_games).where('genred_games.genre_id = 1').references(:genred_games).order(:name) #action-rpg
+    #   elsif sort_attribute_2 == "2"
+    #     @games = Game.includes(:genred_games).where('genred_games.genre_id = 2').references(:genred_games).order(:name) #fps
+    #   elsif sort_attribute_2 == "3"
+    #     @games = Game.includes(:genred_games).where('genred_games.genre_id = 3').references(:genred_games).order(:name) #survival-horror
+    #   elsif sort_attribute_2 == "4"
+    #     @games = Game.includes(:genred_games).where('genred_games.genre_id = 4').references(:genred_games).order(:name) #action-rpg
+    #   end
+    # else
+    #   @games = Game.order(:name)
+    # end
 
   end
 
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,29 +1,50 @@
-class Game < ApplicationRecord
+# class Game < ApplicationRecord
 
-  has_many :user_games
-  has_many :users, through: :user_games
+#   has_many :user_games
+#   has_many :users, through: :user_games
 
-  has_many :platformed_games
-  has_many :platforms, through: :platformed_games
+#   has_many :platformed_games
+#   has_many :platforms, through: :platformed_games
 
-  has_many :genred_games
-  has_many :genres, through: :genred_games
+#   has_many :genred_games
+#   has_many :genres, through: :genred_games
 
-  has_many :user_genres
-  has_many :users, through: :user_genres
-  has_many :genres, through: :user_genres
+#   has_many :user_genres
+#   has_many :users, through: :user_genres
+#   has_many :genres, through: :user_genres
 
-  def lowest_deals
-    platform_deals = []
+#   def lowest_deals
+#     platform_deals = []
 
-    self.platformed_games.each do |platformed_game|
-      if platformed_game.deals.where(active: true).any?
-        platform_deals << { platform_name: platformed_game.platform.name , price: platformed_game.deals.where(active: true).order(:price).first.price, platformed_game_id: platformed_game.id, deal_id: platformed_game.deals.where(active: true).order(:price).first.id }
-      end
-    end
+#     self.platformed_games.each do |platformed_game|
+#       if platformed_game.deals.where(active: true).any?
+#         platform_deals << { platform_name: platformed_game.platform.name , price: platformed_game.deals.where(active: true).order(:price).first.price, platformed_game_id: platformed_game.id, deal_id: platformed_game.deals.where(active: true).order(:price).first.id }
+#       end
+#     end
     
-    return platform_deals
+#     return platform_deals
+
+#   end
+  
+# end
+
+class Game
+
+  attr_accessor :id, :name, :slug, :created_at, :updated_at, :summary, :storyline, :genres, :release_dates
+
+  def initialize(game)
+
+    
+    @id = game["id"]
+    @name = game["name"]
+    @slug = game["slug"]
+    @created_at = game["created_at"]
+    @updated_at = game["updated_at"]
+    @summary = game["summary"]
+    @storyline = game["storyline"]
+    @genres = game["genres"]
+    @release_dates = game["release_dates"]
 
   end
-  
+
 end
