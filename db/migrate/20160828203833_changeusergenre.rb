ad377e9ff8c46baacef20edbedc8a7d733642789
O'Hoon Kwon <ohkwon@ucdavis.edu>
2017-01-05 13:18:41 -0800

    fix migration issue

--- db/migrate/20160828203833_changeusergenre.rb
+++ db/migrate/20160828203833_changeusergenre.rb
@@ -1,6 +1,6 @@
 class Changeusergenre < ActiveRecord::Migration[5.0]
   def change
-    rename_column :user_genres, :user_id_integer, :user_id
+    # rename_column :user_genres, :user_id_integer, :user_id
     # change_column :user_genres, :user_id, :integer
   end
 end
